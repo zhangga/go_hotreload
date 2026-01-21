f83f5443b77e4bb1c12bc1d5dcc326517945085a
zhangzeqiang.game <383523842@qq.com>
2026-01-21 17:46:41 +0800

    add CMakeLists

--- patch/lookup.go
+++ patch/lookup.go
@@ -1,1 +1,13 @@
+//go:build (!linux && !freebsd && !darwin) || !cgo
+
 package patch
+
+import "reflect"
+
+func (b *BasePatchEntry) MakeValueByFunctionName(target any, name string) (reflect.Value, error) {
+	panic("unsupported platform")
+}
+
+func (b *BasePatchEntry) MakeValueByFunctionName4So(target any, soPath, name string) (reflect.Value, error) {
+	panic("unsupported platform")
+}
